2c91d0d7c98024b47f475396f037ddbd78eff5e4
Sourabh Gupta <sourabh.gupta@encoderapps.com>
2026-04-21 19:00:09 +0530

    lwc component with controller class, flow and package.xml

---
+++ .sfdx/typings/lwc/apex/TemplateProcessor.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/TemplateProcessor.generateHtmlFromTemplate" {
+  export default function generateHtmlFromTemplate(param: {accountId: any, templateId: any, mappingId: any}): Promise<any>;
+}
