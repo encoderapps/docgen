42911077900a6a1b3a79fa7a16e285a84b9e68c9
Sourabh Gupta <sourabh.gupta@encoderapps.com>
2026-04-24 14:32:07 +0530

    Remove .sfdx generated files and add to gitignore

--- .sfdx/typings/lwc/apex/TemplateProcessor.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/TemplateProcessor.generateHtmlFromTemplate" {
-  export default function generateHtmlFromTemplate(param: {accountId: any, templateId: any, mappingId: any}): Promise<any>;
-}
